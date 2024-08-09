d0553cf6ec740745eefcf5d37f43af1de96695d2
Suyash Pandey <92623123+SP4567@users.noreply.github.com>
2024-08-09 11:53:31 +0530

    Update Predict.py

--- Predict.py
+++ Predict.py
@@ -35,7 +35,7 @@ def return_prediction(model, Scaler, sample_json):
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Build the full path to the vectorizer file
-scaler_path = os.path.join(base_dir, 'vectorizer (3).pkl')
+scaler_path = os.path.join(base_dir, 'scaler (3).pkl')
 
 # Load the vectorizer
 with open(vectorizer_path, 'rb') as f:
@@ -45,25 +45,25 @@ with open(vectorizer_path, 'rb') as f:
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Build the full path to the model file
-model_path = os.path.join(base_dir, 'Spam_classifier.h5')
+model_path = os.path.join(base_dir, 'Heart_Disease_Predictor (1).h5')
 
 #loading the model
 model = load_model(model_path)
 
 st.title("Heart_Disease_Predictor")
 a = st.number_input('Enter the age', step=1., format="%.2f")
-s = st.number_input('Enter the sex', step=1., format="%.2f")
-cp = st.number_input('Enter the cp',  step=1., format="%.2f")
-c = st.number_input('Enter the trestbps', step=1., format="%.2f")
-o = st.number_input('Enter the chol', step=1., format="%.2f")
-f = st.number_input('Enter the fbs', step=1., format="%.2f")
-r = st.number_input('Enter the restecg', step=1., format="%.2f")
-t = st.number_input('Enter the thalach', step=1., format="%.2f")
-e = st.number_input('Enter the exang',  step=1., format="%.2f")
-p = st.number_input('Enter the oldpeak', step=1., format="%.2f")
-slope = st.number_input('Enter the slope', step=1., format="%.2f")
-v = st.number_input('Enter the ca',  step=1., format="%.2f")
-l = st.number_input('Enter the thal', step=1., format="%.2f")
+s = st.number_input('Enter the sex, (enter 1 for male, 0 for female)', step=1., format="%.2f")
+cp = st.number_input('Enter the chest pain type (1: if there is pain, 0: if not)',  step=1., format="%.2f")
+c = st.number_input('Enter the trestbps (resting blood pressure (in mm Hg))', step=1., format="%.2f")
+o = st.number_input('Enter the cholestrol (serum cholesterol in mg/dl)', step=1., format="%.2f")
+f = st.number_input('Enter the fbs ("fasting blood sugar," typically represented as 1 (true) if > 120 mg/dl and 0 (false) otherwise)', step=1., format="%.2f")
+r = st.number_input('Enter the restecg (resting electrocardiographic measurement, 0: Normal, 1: Having ST-T wave abnormality, 2: Abnormal)', step=1., format="%.2f")
+t = st.number_input('Enter the thalach (maximum heart rate achieved during exercise)', step=1., format="%.2f")
+e = st.number_input('Enter the exang (exercise-induced angina, 1: yes and 0: no)',  step=1., format="%.2f")
+p = st.number_input('Enter the oldpeak (ST depression induced by exercise relative to rest)', step=1., format="%.2f")
+slope = st.number_input('Enter the slope (slope of the peak exercise ST segment)', step=1., format="%.2f")
+v = st.number_input('Enter the ca (number of major vessels colored by fluoroscopy)',  step=1., format="%.2f")
+l = st.number_input('Enter the thal (thalassemia condition)', step=1., format="%.2f")
 if st.button('Predict'):
     dc = [[a, s, cp, c, o, f, r, t, e, p, slope, v, l]]
     inp = Scaler.fit_transform(dc)
